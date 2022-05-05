269a246ebf083c9d36ba68a97756ba8e2ee9c9b3
1ChoiChoi2 <khanhduc.nguyenhoang12@gmail.com>
2022-05-05 22:24:42 +0700

    hello

---
+++ BasicPy/index.py
@@ -0,0 +1,11 @@
+from re import X
+
+
+print('Hello World')
+print('Hello World')
+print('Hello World')
+print('Go go')
+
+x = 9 + 6
+
+print(x)
--- README.md
+++
@@ -1,2 +0,0 @@
-# Learning-Front-End-Project
-# Learning-Front-End-Project
